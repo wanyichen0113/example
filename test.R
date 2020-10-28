48e7f9bcf46d458a0162e783710b6d219e9b98d2
wanyichen0113 <ucfnenm@ucl.ac.uk>
2020-10-28 15:23:25 +0800

    changed again
    again

--- test.R
+++ test.R
@@ -3,3 +3,14 @@ y <- 2
 z <- x + y
 p <- x * 2
 n = y * 3
+install.packages("reprex")
+library(reprex)
+A <- 1
+B <- 2
+C <- A+B
+C
+reprex()
+#stackoverflow
+reprex(venue="so")
+#slack
+reprex(venue="r")
